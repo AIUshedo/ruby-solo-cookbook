26639b99544440826aa1074966d6c430f2aeb682
unknown <aushedo@spartaglobal.com>
2019-02-26 15:45:06 +0000

    ruby done

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+include_recipe("apt")
+
+apt_update("update") do
+  action :update
+end
+
+package "ruby" do
+  action :install
+end
+
+package "ruby" do
+  action :upgrade
+end
