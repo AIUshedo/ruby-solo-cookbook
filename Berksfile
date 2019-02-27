c16be41d70906af94d08ead378cd5005d24f528c
unknown <aushedo@spartaglobal.com>
2019-02-27 11:19:56 +0000

    dinsndoid

--- Berksfile
+++ Berksfile
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 source 'https://supermarket.chef.io'
-
+cookbook 'ruby', git: 'git@github.com:AIUshedo/ruby-solo-cookbook.git'
 metadata
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -7,12 +7,9 @@
 
 unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
+  describe package "ruby" do
+    it { should be_installed }
   end
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
-end
